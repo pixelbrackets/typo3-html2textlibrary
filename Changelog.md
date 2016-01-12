7fe280821cda68538d512d9d46d387a7bdff6d22
Dan <mail@pixelbrackets.de>
2016-01-12 10:03:09 +0100

    [TASK] Initial commit
    Fork EXT:ods_plaintext

---
+++ Changelog.md
@@ -0,0 +1,8 @@
+Changelog
+=========
+
+2016-01-12 Dan Untenzu <untenzu@webit.de>
+
+  * 1.0.0
+  * TASK Fork EXT:ods_plaintext
+  * TASK Update html2text script
---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'HTML to TXT Library',
+	'description' => 'Creates a plain text version of a webpage using the html2text library',
+	'category' => 'misc',
+	'author' => 'Dan Untenzu',
+	'author_email' => 'untenzu@webit.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.6.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
