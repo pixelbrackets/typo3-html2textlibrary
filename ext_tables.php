9a567a4efd77de019facd25abfa00e0482d95ed9
Dan Untenzu <untenzu@webit.de>
2016-01-12 10:10:00 +0100

    [FEATURE] Update Html2Text script
    * Update Html2Text Library
    * Rename files & classes

--- ext_tables.php
+++ ext_tables.php
@@ -2,5 +2,5 @@
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/ods_plaintext/', 'Plaintext for direct_mail with templavoila');
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/', 'Plaintext for direct_mail with templavoila');
 ?>
