4975c5c90203bde20c271245dedac28e1620e4dc
Paul Luding <luding@webit.de>
2020-07-01 10:46:17 +0200

    [FEATURE] Update dependencies
    * Drop support for TYPO3 7
    * Add support for TYPO3 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => []
